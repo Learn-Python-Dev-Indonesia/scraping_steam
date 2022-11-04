0bffb1ad01d4069338bfb3e21c253b01371a1a29
viko-aldi <piccoaldi@gmail.com>
2022-11-04 19:45:45 +0700

    First Commit

--- main.py
+++ main.py
@@ -1,64 +1,16 @@
-import os
+
 import requests
 from bs4 import BeautifulSoup
-import json
-import pandas as pd
 
-url = 'https://store.steampowered.com/search/?term=gta'
-
-def get_data(url):
-    r = requests.get(url)
-    return r.text
-
-# Processing Data
-def parse(data):
-    result = []
-    soup = BeautifulSoup(data, 'html.parser')
-    try:
-        os.mkdir('json_result')
-    except FileExistsError:
-        pass
-    contents = soup.find('div', attrs={'id': 'search_resultsRows'})
-    games = contents.find_all('a')
-
-    for game in games:
-        link = game['href']
-
-#parsing data
-        title = game.find('span', {'class': 'title'}).text
-        price = game.find('div', {'class': 'col search_price  responsive_secondrow'})
-
-
-
-        data_dict = {
-            'title': title,
-            'price': price,
-            'link' : link,
-        }
-
-        result.append(data_dict)
-    return result
-
-    with open('json_result.json', 'w') as outfile:
-        json.dump(result, outfile)
-    return result
-
-def load_data():
-    with open('json_result.json') as  json_file:
-        data = json.load(json_file)
-
-def output (datas : list):
-    for i in datas:
-        print(i)
-def generate_data(result, filename):
-    df = pd.DataFrame(result)
-    df.to_excel(f'{filename}.xlsx', index=False)
-
-if __name__ == '__main__':
-    data = get_data(url)
-
-    final_data = parse(data)
-    namafile = input('Masukkan nama file:')
-    generate_data(final_data, namafile)
-
-    output(final_data)
+url = 'https://indeed.com/jobs?'
+params = {
+    'q': 'python developer',
+    'l': 'Jakarta',
+    'from': 'searchOnHP',
+    'vjk': 'b287232987f71d31',
+}
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+}
+res = requests.get(url, params=params, headers=headers)
+print(res.status_code)
